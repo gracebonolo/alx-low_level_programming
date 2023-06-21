e7f80a5b80fa37764ab9897b8aa8b9b21bca908c
Grace Mtaje <gracemtaje5@gmail.com>
2023-06-21 12:28:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,20 @@
 #include "main.h"
 
 /**
+ * _putchar - custom function to print a single character
+ * @c: the character to be printed
+ *
+ * Return: On success, returns the character that was printed.
+ *         On error, returns -1.
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
+/**
  * main - prints _putchar and a new line
+ *
  * Return: Always 0
  */
 int main(void)
@@ -14,8 +27,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('/n');
-
+	_putchar('\n');
 
 	return (0);
 }
