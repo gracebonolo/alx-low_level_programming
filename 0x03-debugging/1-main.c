eb6e7a1c9bcf74c8ef09fcf7c6fac4fa10bace47
Grace Mtaje <gracemtaje5@gmail.com>
2023-06-23 15:27:43 -0700

    like comment subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
+	printf("Infinite loop avoided: \\0/\n");
+
+	return (0);
+}
