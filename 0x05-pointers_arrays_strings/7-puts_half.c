2653973ddea7fce757e6cc8abd4e4b17d2a7c2e9
Grace Mtaje <gracemtaje5@gmail.com>
2023-06-27 09:59:13 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * puts_half - Prints the second half of a string.
  * @str: The string to print the second half of.
  */
