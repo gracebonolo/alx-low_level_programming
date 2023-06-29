52aea7fcdf2c9572a43f572edc11171c8acd2773
Grace Mtaje <gracemtaje5@gmail.com>
2023-06-29 07:57:41 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
- * Return: s1[i] - s2[i]
+ * Return: difference between s1[i] and s2[i]
  */
-int _strcmp(char *s1, cahr *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
