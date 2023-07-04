6ebf146299ab6822a55b2d0cc25279c824a48397
Grace Mtaje <gracemtaje5@gmail.com>
2023-07-04 06:27:54 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int t);
 void jack_bauer(void);
-void time_table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void _putchar(char c);
