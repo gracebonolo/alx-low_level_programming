51c63c9d76030321d3d0b0cdd27744a694b6e4f7
Grace Mtaje <gracemtaje5@gmail.com>
2023-07-04 17:03:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,8 +15,7 @@
  *		block is large enough to accommodate 'n' bytes and does not
  *		perform any size checks.
  */
-char *_memset(char *s, char b, unsigned int n)
-{
+char *_memset(char *s, char *b, unsigned int n) {
 	int i = 0;
 
 	for (; n > 0; i++)
