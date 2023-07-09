168e6a3485d97f1052a22ebf00d4ecb319ac39c2
Grace Mtaje <gracemtaje5@gmail.com>
2023-07-09 09:23:36 -0700

    isupper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check if uppercase letters
+ * @c: char to check
+ *
+ * Return: if uppercase returns 1, else return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (0);
+
+	return (0);
+}
