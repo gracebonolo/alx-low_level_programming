709c3e912f50e728c19459725803858581d078b9
Grace Mtaje <gracemtaje5@gmail.com>
2023-07-19 06:20:07 -0700

    array iterartor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	for (size_t i = 0; i < size; i++)
+	size_t i;
+
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
+
