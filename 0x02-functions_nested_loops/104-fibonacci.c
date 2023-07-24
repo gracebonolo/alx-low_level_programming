47cdb682eae3011438e1ca4a1d224757d7def1b0
gracebonolo <gracemtaje5@gmail.com>
2023-07-24 12:18:36 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count, n;
+	unsigned long fib1 = 1, fib2 = 2, next;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (count = 2; count < 98; count++)
+	{
+		next = fib1 + fib2;
+		printf(", %lu", next);
+		fib1 = fib2;
+		fib2 = next;
+	}
+
+	printf("\n");
+	return (0);
+}
+
