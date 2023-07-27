74145471e00ceb81ec191576e5c539ec13f50215
gracebonolo <gracemtaje5@gmail.com>
2023-07-27 14:35:08 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-    hello_message db "Hello, Holberton", 0
-    format db "%s", 10, 0 ; "%s" followed by a new line character (10) and a null terminator (0)
+    hello_message db "Hello, Holberton", 10, 0 ; "Hello, Holberton" followed by a new line character (10) and a null terminator (0)
+    format db "%s", 0
 
 section .text
     extern printf
@@ -8,13 +8,13 @@ section .text
 global main
 main:
     ; Prepare the arguments for printf
-    mov rdi, format     ; First argument: format string
+    mov rdi, format   ; First argument: format string
     mov rsi, hello_message ; Second argument: address of the hello_message string
-    xor rax, rax       ; Clear RAX register (set to 0) to indicate no floating-point arguments
+    xor rax, rax     ; Clear RAX register (set to 0) to indicate no floating-point arguments
     call printf
 
     ; Exit the program
-    mov rax, 60        ; syscall number for exit
-    xor rdi, rdi       ; status = 0
+    mov rax, 60      ; syscall number for exit
+    xor rdi, rdi     ; status = 0
     syscall
 
