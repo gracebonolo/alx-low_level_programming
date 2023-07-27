87155fbb2c078853067bdd87a4be832918889b21
gracebonolo <gracemtaje5@gmail.com>
2023-07-27 14:19:39 -0700

    all files that contain 4-list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
