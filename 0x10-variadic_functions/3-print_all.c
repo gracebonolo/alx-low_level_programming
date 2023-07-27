1fbb8026f86a4a0639880f296d899507f38a7ba3
gracebonolo <gracemtaje5@gmail.com>
2023-07-27 15:12:26 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,16 +1,19 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_all - Prints anything based on the format provided.
+ * @format: A list of types of arguments passed to the function.
+ *
+ * Return: void.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str, *separator = "";
+	char *separator = "";
+	char *str;
 
 	va_start(args, format);
 
@@ -18,23 +21,27 @@ void print_all(const char * const format, ...)
 	{
 		switch (format[i])
 		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", separator, str);
-				break;
+		case 'c':
+			printf("%s%c", separator, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", separator, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", separator, va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", separator, str);
+			break;
+		default:
+			i++;
+			continue;
 		}
-		separator = ", ";
+
+		separator = ",";
 		i++;
 	}
 
