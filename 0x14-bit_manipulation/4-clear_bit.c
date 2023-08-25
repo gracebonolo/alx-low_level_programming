7954b468e920ab5fb0ee38ea25c46cafa17c3ba2
Grace Mtaje <gracemtaje5@.com>
2023-08-25 15:18:58 -0700

    a clear bit file consiting of c programming bit mani

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit to 0 at a given index.
- * @n: Pointer to the number.
+ * clear_bit - Clears a bit at a given index in a number.
+ * @n: A pointer to the number.
  * @index: The index of the bit to clear.
  *
  * Return: 1 if it worked, or -1 if an error occurred.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
-	*n &= ~mask;
+	mask = 1 << index;
+	*n = *n & ~mask;
 
 	return (1);
 }
+
