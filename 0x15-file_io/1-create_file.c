37f38be48a2c86cae6e18a3e1933f69094be7cf1
Grace Mtaje <gracemtaje5@gmail.com>
2023-12-06 07:11:42 -0800

    al files for testing for project file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - Creates a file with a specified text content.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_result, close_result;
+	mode_t permissions = S_IRUSR | S_IWUSR; /* rw------- */
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write_result = write(file_descriptor, text_content, _strlen(text_content));
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close_result = close(file_descriptor);
+	if (close_result == -1)
+		return (-1);
+
+	return (1);
+}
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: The string to compute the length of.
+ *
+ * Return: The length of the string.
+ */
+size_t _strlen(char *s)
+{
+	size_t len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
