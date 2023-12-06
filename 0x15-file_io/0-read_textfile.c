c85a28604419a53cf0af0f9b65dd5b078ef9a8a1
Grace Mtaje <gracemtaje5@gmail.com>
2023-12-06 07:02:30 -0800

    the file task for file i/o that has a poem in it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a specified number of letters from a text file and prints them to the standard output.
+ * @filename: The name of the file to be read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed. If an error occurs, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		fprintf(stderr, "Error: Filename is NULL\n");
+		return (0);
+	}
+
+	FILE *file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return (0);
+	}
+
+	char *buffer = (char *)malloc(letters + 1); /* Allocate space for the buffer */
+	if (buffer == NULL)
+	{
+		perror("Error allocating buffer");
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t bytesRead = fread(buffer, 1, letters, file);
+	if (bytesRead == -1)
+	{
+		perror("Error reading file");
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	buffer[bytesRead] = '\0'; /* Null-terminate the string */
+
+	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1 || (size_t)bytesWritten != (size_t)bytesRead)
+	{
+		perror("Error writing to stdout");
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	free(buffer);
+	fclose(file);
+
+	return (bytesRead);
+}
+
