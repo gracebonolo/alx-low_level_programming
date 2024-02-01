6cc4e581bf0dbeb7b4218e3f39e0afdfc83f6af1
Grace Mtaje <gracemtaje5@gmail.com>
2024-02-01 12:39:12 -0800

    all files for python

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+/**
+ * hash_djb2 - Hashes a string using the djb2 algorithm.
+ * @str: The input string to be hashed.
+ *
+ * Return: The hash value.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
+
